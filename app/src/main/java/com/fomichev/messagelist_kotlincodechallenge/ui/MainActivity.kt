8311ed6e27eb34300f2bda4b82b874be1cf725ea
Sergey Fomichev <7554732@gmail.com>
2020-12-16 10:15:48 +0300

    move MainActivity to ui

--- app/src/main/java/com/fomichev/messagelist_kotlincodechallenge/ui/MainActivity.kt
+++ app/src/main/java/com/fomichev/messagelist_kotlincodechallenge/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.fomichev.messagelist_kotlincodechallenge
+package com.fomichev.messagelist_kotlincodechallenge.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fomichev.messagelist_kotlincodechallenge.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
